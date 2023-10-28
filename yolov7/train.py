3c9ea1b876d2a4643a1140995143b3175450889d
Hannah DeFazio <hannah.defazio@gyges.khq.kitware.com>
2023-10-28 17:37:27 -0400

    Allow training to use all gpus on gyges

--- yolov7/train.py
+++ yolov7/train.py
@@ -1,7 +1,7 @@
 import os
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="1, 2, 3"
+os.environ["CUDA_VISIBLE_DEVICES"]="0, 1, 2, 3"
 
 import argparse
 import logging
