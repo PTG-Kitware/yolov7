967ced97969fe0534f5d55ee7932868dc92301ed
Hannah DeFazio <hannah.defazio@gyges.khq.kitware.com>
2024-04-17 17:15:09 -0400

    Remove yolov8; function moved to the angel system

--- yolov7/detect_ptg.py
+++ yolov7/detect_ptg.py
@@ -25,7 +25,6 @@ from yolov7.utils.torch_utils import select_device, TracedModel
 from yolov7.utils.plots import plot_one_box
 from yolov7.utils.datasets import letterbox
 
-from ultralytics import YOLO
 import moviepy.video.io.ImageSequenceClip
 
 """
@@ -141,45 +140,7 @@ def load_model(device, weights_fp, img_size):
     stride = int(model.stride.max())  # model stride
     imgsz = check_img_size(img_size, s=stride)  # check img_size
     return device, model, stride, imgsz
-
-def predict_hands(hand_model: YOLO, img0: np.array, device: str) -> tuple:
-    width, height = img0.shape[:2]
-    hands_preds = hand_model.predict(
-                                        source=img0,
-                                        conf=0.1,
-                                        imgsz=width,
-                                        device=device,
-                                        verbose=False)[0] # list of length=num images
     
-    hand_centers = [center.xywh.tolist()[0][0] for center in hands_preds.boxes][:2]
-    hands_label = []
-    
-    if len(hand_centers) == 2:
-        if hand_centers[0] > hand_centers[1]:
-            hands_label.append("hand (right)")
-            hands_label.append("hand (left)")
-        elif hand_centers[0] <= hand_centers[1]:
-            hands_label.append("hand (left)")
-            hands_label.append("hand (right)")
-    elif len(hand_centers) == 1:
-        if hand_centers[0] > width//2:
-            hands_label.append("hand (right)")
-        elif hand_centers[0] <= width//2:
-            hands_label.append("hand (left)")
-
-    boxes, labels, confs = [], [], []
-    
-    for bbox, hand_cid in zip(hands_preds.boxes, hands_label):
-        
-        xyxy_hand = bbox.xyxy.tolist()[0]
-
-        conf = bbox.conf.item()
-        
-        boxes.append(xyxy_hand)
-        labels.append(hand_cid)
-        confs.append(conf)
-
-    return boxes, labels, confs
 
 def predict_image(
     img0: npt.NDArray,
